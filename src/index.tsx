b0245aba79489b6a1b70b5cdac4b7e78e5735923
felipe <profissionalf.andre@gmail.com>
2024-09-13 08:24:10 -0300

    refactor: Remove commented out code in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// function Test() {
-//   const [rating, setRating] = useState(0);
-//   return (
-//     <>
-//       <StarRating
-//         maxRating={10}
-//         messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-//         defaultRating={5}
-//         onSetRating={setRating}
-//       />
-//       rating {rating}
-//     </>
-//   );
-// }
-
 root.render(
   <React.StrictMode>
     <App />
